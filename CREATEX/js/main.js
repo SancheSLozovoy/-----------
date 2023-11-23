d23a4f6c716feaef967c8061be4a0f677aa212b0
SancheSLozovoy <alex.lozovoy05@bk.ru>
2023-11-23 23:46:42 +0300

    commit

--- CREATEX/js/main.js
+++ CREATEX/js/main.js
@@ -25,5 +25,40 @@ $(function () {
         })
     })
 
+    $('.testimonials__slider').slick({
+        arrows : false,
+        dots: true,
+        appendDots: $('.testimonials__dots'),
+    })
 
+    $('.testimonials__prev').on('click', function (e){
+        e.preventDefault()
+        $('.testimonials__slider').slick('slickPrev')({  
+        })
+    })
+    $('.testimonials__next').on('click', function (e){
+        e.preventDefault()
+        $('.testimonials__slider').slick('slickNext')({  
+        })
+    })
+
+
+
+    // $('.program__acc-link').on('click', function (e){
+    //     e.preventDefault()
+    //     $(this).toggleClass('program__acc-link--active')
+    //     $(this).children('.program__acc-text').slideToggle()
+    // })
+    $('.program__acc-link').on('click', function (e){
+        e.preventDefault()
+        if($(this).hasClass('program__acc-link--active')){
+            $(this).removeClass('program__acc-link--active')
+            $(this).children('.program__acc-text').slideUp()
+        }else {
+            $('.program__acc-link').removeClass('program__acc-link--active')
+            $('.program__acc-text').slideUp()
+            $(this).addClass('program__acc-link--active')
+            $(this).children('.program__acc-text').slideDown()
+        }
+    })
 })
